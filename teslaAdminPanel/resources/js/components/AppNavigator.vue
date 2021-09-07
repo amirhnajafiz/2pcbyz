80bc9f81e47a6469cf57735ea4ba1a1551042794
9831065 <najafizadeh21@gmail.com>
2021-09-07 11:12:06 +0430

    Routes refactor

--- teslaAdminPanel/resources/js/components/AppNavigator.vue
+++ teslaAdminPanel/resources/js/components/AppNavigator.vue
@@ -4,28 +4,24 @@
       dark
       src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
     >
-      <v-toolbar-title>
-          Tesla Admin Panel
-      </v-toolbar-title>
+      <v-toolbar-title> Tesla Admin Panel </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn>
+      <v-btn class="mr-2" @click="() => this.$router.push('addCategory')">
+        Add Category
+      </v-btn>
+      <v-btn @click="() => this.$router.push('categories')">
         Categories
       </v-btn>
-      <v-btn class="ml-2">
-        Products
-      </v-btn>
+      <v-btn class="ml-2"> Products </v-btn>
     </v-toolbar>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
