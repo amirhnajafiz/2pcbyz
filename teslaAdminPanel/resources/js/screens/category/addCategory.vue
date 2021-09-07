d5c0f2c08cb4a9305fe2860bb03e1635fea86db5
9831065 <najafizadeh21@gmail.com>
2021-09-07 11:27:13 +0430

    Added models binding in category form

--- teslaAdminPanel/resources/js/screens/category/addCategory.vue
+++ teslaAdminPanel/resources/js/screens/category/addCategory.vue
@@ -1,13 +1,28 @@
 <template>
   <v-container>
     <v-form ref="form">
-      <v-text-field :counter="35" label="Name" required dark></v-text-field>
+      <v-text-field
+        v-model="name"
+        :counter="35"
+        label="Name"
+        required
+        dark
+      ></v-text-field>
 
-      <v-text-field label="Order" required dark></v-text-field>
+      <v-text-field v-model="order" label="Order" required dark></v-text-field>
 
       <v-row class="mb-2">
-          <font-awesome-icon icon="camera" class="white--text mt-4 ml-2" style="font-size: 30px; margin-right: -20px;" />
-        <v-file-input label="Image" dark show-size></v-file-input>
+        <font-awesome-icon
+          icon="camera"
+          class="white--text mt-4 ml-2"
+          style="font-size: 30px; margin-right: -20px"
+        />
+        <v-file-input
+          v-model="image"
+          label="Image"
+          dark
+          show-size
+        ></v-file-input>
       </v-row>
 
       <v-btn color="success" class="mr-4"> Submit </v-btn>
@@ -16,7 +31,15 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "",
+      order: null,
+      image: "",
+    };
+  },
+};
 </script>
 
 <style>
