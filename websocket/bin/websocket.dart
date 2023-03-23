c369368b346f80f73d9fda4d3208f880994ab6b6
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-23 13:36:55 +0330

    add: json encode for incoming data

--- websocket/bin/websocket.dart
+++ websocket/bin/websocket.dart
@@ -1,9 +1,27 @@
+import 'dart:convert';
+
 import 'package:websocket/websocket.dart' as websocket;
 
 void main(List<String> arguments) {
   // Create the WebSocket channel
   final channel = WebSocketChannel.connect(
     Uri.parse('wss://ws-feed.pro.coinbase.com'),
+  );
+
+  channel.sink.add(
+    jsonEncode(
+      {
+        "type": "subscribe",
+        "channels": [
+          {
+            "name": "ticker",
+            "product_ids": [
+              "BTC-EUR",
+            ]
+          }
+        ]
+      },
+    ),
   );
 
   // Listen for all incoming data
