7df99ff871e79576878d25a85fb980f5c67dc15b
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-23 13:35:52 +0330

    add: channel connection

--- websocket/bin/websocket.dart
+++ websocket/bin/websocket.dart
@@ -1,5 +1,16 @@
 import 'package:websocket/websocket.dart' as websocket;
 
 void main(List<String> arguments) {
-  print('Hello world: ${websocket.calculate()}!');
+  // Create the WebSocket channel
+  final channel = WebSocketChannel.connect(
+    Uri.parse('wss://ws-feed.pro.coinbase.com'),
+  );
+
+  // Listen for all incoming data
+  channel.stream.listen(
+        (data) {
+      print(data);
+    },
+    onError: (error) => print(error),
+  );
 }
