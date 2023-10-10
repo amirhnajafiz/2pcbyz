e6aa42ed810fd7a046257b5e38d08c79905080c7
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-10 17:01:20 +0330

    update: main

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	metrics "k8s.io/metrics/pkg/client/clientset/versioned"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	// todo: convert it to read from kube config file
+	var kubeconfig, master string // empty, assuming inClusterConfig
+	config, err := clientcmd.BuildConfigFromFlags(master, kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	mc, err := metrics.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	mc.MetricsV1beta1().NodeMetricses().List(metav1.ListOptions{})
 }
