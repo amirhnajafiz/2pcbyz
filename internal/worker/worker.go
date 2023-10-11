9799e50870af13435c871358ee0791f530505857
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-11 11:25:11 +0330

    update: worker with metrics

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 	"log"
 
+	handler "github.com/amirhnajafiz/node-exporter/internal/metrics"
+
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	metrics "k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
-type Worker struct{}
+type Worker struct {
+	Metrics handler.Metrics
+}
 
 func (w Worker) Work(cfg *rest.Config) error {
 	// create new metrics server
