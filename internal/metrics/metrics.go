f0d1bbb62fb0e46f203a4c4a6b0ff822d28fc1fb
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-11 11:50:07 +0330

    add: metrics methods

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,19 +1,26 @@
 package metrics
 
-type Metrics struct{}
+import "github.com/prometheus/client_golang/prometheus"
+
+type Metrics struct {
+	CPU     prometheus.HistogramVec
+	Memory  prometheus.HistogramVec
+	Pods    prometheus.HistogramVec
+	Storage prometheus.HistogramVec
+}
 
 func (m *Metrics) ObserveCPU(label string, value int64) {
-
+	m.CPU.With(prometheus.Labels{"name": label}).Observe(float64(value))
 }
 
 func (m *Metrics) ObserveMemory(label string, value int64) {
-
+	m.Memory.With(prometheus.Labels{"name": label}).Observe(float64(value))
 }
 
 func (m *Metrics) ObservePods(label string, value int64) {
-
+	m.Pods.With(prometheus.Labels{"name": label}).Observe(float64(value))
 }
 
 func (m *Metrics) ObserveStorage(label string, value int64) {
-
+	m.Storage.With(prometheus.Labels{"name": label}).Observe(float64(value))
 }
