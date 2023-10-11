9cad10ad1479b002a1a63e526fbb7cdc6d5dcf6e
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-11 11:53:44 +0330

    add: new metrics instance

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -3,10 +3,39 @@ package metrics
 import "github.com/prometheus/client_golang/prometheus"
 
 type Metrics struct {
-	CPU     prometheus.HistogramVec
-	Memory  prometheus.HistogramVec
-	Pods    prometheus.HistogramVec
-	Storage prometheus.HistogramVec
+	CPU     *prometheus.HistogramVec
+	Memory  *prometheus.HistogramVec
+	Pods    *prometheus.GaugeVec
+	Storage *prometheus.HistogramVec
+}
+
+func New() *Metrics {
+	return &Metrics{
+		CPU: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Name: "cpu_usage",
+			},
+			[]string{"name"},
+		),
+		Memory: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Name: "memory_usage",
+			},
+			[]string{"name"},
+		),
+		Pods: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "total_pods",
+			},
+			[]string{"name"},
+		),
+		Storage: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Name: "storage_usage",
+			},
+			[]string{"name"},
+		),
+	}
 }
 
 func (m *Metrics) ObserveCPU(label string, value int64) {
@@ -18,7 +47,7 @@ func (m *Metrics) ObserveMemory(label string, value int64) {
 }
 
 func (m *Metrics) ObservePods(label string, value int64) {
-	m.Pods.With(prometheus.Labels{"name": label}).Observe(float64(value))
+	m.Pods.With(prometheus.Labels{"name": label}).Set(float64(value))
 }
 
 func (m *Metrics) ObserveStorage(label string, value int64) {
