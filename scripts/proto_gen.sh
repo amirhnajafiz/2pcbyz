7006f985b1797b9eb3a6ea0de8a6f4513883361f
amirhnajafiz <najafizadeh21@gmail.com>
2024-12-13 06:05:50 -0500

    fix: proto export file

--- scripts/proto_gen.sh
+++ scripts/proto_gen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-protoc -I=proto/ --go_out=cluster/ proto/pbft.proto
-protoc -I=proto/ --go-grpc_out=cluster/ proto/pbft.proto
-protoc -I=proto/ --go_out=cluster/ proto/database.proto
-protoc -I=proto/ --go-grpc_out=cluster/ proto/database.proto
-protoc -I=proto/ --go_out=cluster/ proto/api.proto
-protoc -I=proto/ --go-grpc_out=cluster/ proto/api.proto
-protoc -I=proto/ --go_out=client/ proto/database.proto
-protoc -I=proto/ --go-grpc_out=client/ proto/database.proto
-protoc -I=proto/ --go_out=client/ proto/api.proto
-protoc -I=proto/ --go-grpc_out=client/ proto/api.proto
+protoc -I=proto/ --go_out=cluster/pkg/ proto/pbft.proto
+protoc -I=proto/ --go-grpc_out=cluster/pkg/ proto/pbft.proto
+protoc -I=proto/ --go_out=cluster/pkg/ proto/database.proto
+protoc -I=proto/ --go-grpc_out=cluster/pkg/ proto/database.proto
+protoc -I=proto/ --go_out=cluster/pkg/ proto/api.proto
+protoc -I=proto/ --go-grpc_out=cluster/pkg/ proto/api.proto
+protoc -I=proto/ --go_out=client/pkg/ proto/database.proto
+protoc -I=proto/ --go-grpc_out=client/pkg/ proto/database.proto
+protoc -I=proto/ --go_out=client/pkg/ proto/api.proto
+protoc -I=proto/ --go-grpc_out=client/pkg/ proto/api.proto
