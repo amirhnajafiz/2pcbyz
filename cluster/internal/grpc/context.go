b804c59816364329dbad942920e5f660c65820e3
amirhnajafiz <najafizadeh21@gmail.com>
2024-12-13 19:12:53 -0500

    update: grpc inputs in cluster

---
+++ cluster/internal/grpc/context.go
@@ -0,0 +1,8 @@
+package grpc
+
+// contextKey is used in order to store key-value pairs inside a context
+type contextKey string
+
+func (c contextKey) String() string {
+	return string(c)
+}
--- cluster/internal/grpc/interceptors.go
+++ cluster/internal/grpc/interceptors.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"strings"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -15,7 +16,6 @@ func (b *Bootstrap) allStreamInterceptor(
 	handler grpc.StreamHandler,
 ) error {
 	b.Logger.Info("stream rpc called", zap.String("method", info.FullMethod))
-
 	return handler(srv, ss)
 }
 
@@ -27,6 +27,34 @@ func (b *Bootstrap) allUnaryInterceptor(
 	handler grpc.UnaryHandler,
 ) (interface{}, error) {
 	b.Logger.Info("rpc called", zap.String("method", info.FullMethod))
+	return b.checkEmptyReturnCallsInterceptor(ctx, req, info, handler)
+}
+
+// checkEmptyReturnCallsInterceptor accepts requests from the all unary interceptor
+// and passes the database and pbft requests inside the handler queue.
+func (b *Bootstrap) checkEmptyReturnCallsInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	// parse to get service and method
+	if svc, method, err := parseFullMethod(info.FullMethod); err == nil {
+		// if the service is database or pbft, it is an empty return call
+		if strings.HasPrefix(svc, "database") || strings.HasPrefix(svc, "pbft") {
+			b.Queue <- context.WithValue(
+				context.WithValue(
+					context.Background(),
+					contextKey("method"),
+					method,
+				),
+				contextKey("request"),
+				req,
+			)
+
+			return nil, nil
+		}
+	}
 
 	return handler(ctx, req)
 }
