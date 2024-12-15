f348ec158f737d250d286fdd0f8d89307a203cac
amirhnajafiz <najafizadeh21@gmail.com>
2024-12-14 20:44:41 -0500

    add: cluster models

---
+++ cluster/internal/models/transaction.go
@@ -0,0 +1,10 @@
+package models
+
+// Transaction is a bson model to store the client requests.
+type Transaction struct {
+	Sender    string `bson:"sender"`
+	Receiver  string `bson:"receiver"`
+	Amount    int    `bson:"amount"`
+	SessionId int    `bson:"session_id"`
+	Status    string `bson:"status"`
+}
